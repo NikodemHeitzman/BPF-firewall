4ea65ab89f1aeb6b80a77632f15434834ff26149
nikodem <nikodem.heitzman2002@gmail.com>
2026-08-01 17:39:38 +0200

    create Control Plane

--- firewall/src/main.rs
+++ firewall/src/main.rs
@@ -1,13 +1,18 @@
+use aya::maps::HashMap;
 use aya::programs::{Xdp, XdpFlags};
-use aya::Ebpf;
+use aya::{include_bytes_aligned, Bpf};
 use clap::Parser;
 use log::{info, warn};
+use std::net::Ipv4Addr;
 use tokio::signal;
 
 #[derive(Debug, Parser)]
 struct Opt {
     #[clap(short, long, default_value = "eth0")]
     iface: String,
+
+    #[clap(short, long)]
+    block_ip: Option<String>,
 }
 
 #[tokio::main]
@@ -15,27 +20,28 @@ async fn main() -> Result<(), anyhow::Error> {
     let opt = Opt::parse();
     env_logger::init();
 
-    // Ładowanie skompilowanego bajtkodu eBPF z Data Plane
-    // W środowisku produkcyjnym ścieżka będzie inna.
-    let mut bpf = Ebpf::load_file("../target/bpfel-unknown-none/release/firewall-ebpf")?;
+    let mut bpf = Bpf::load_file("../target/bpfel-unknown-none/release/firewall-ebpf")?;
 
-    // Inicjalizacja logowania z poziomu kernela (aya-log)
     if let Err(e) = aya_log::EbpfLogger::init(&mut bpf) {
         warn!("Nie udało się zainicjować logowania eBPF: {}", e);
     }
 
     let program: &mut Xdp = bpf.program_mut("firewall").unwrap().try_into()?;
     program.load()?;
-
-    // Podpięcie programu pod interfejs
     program
         .attach(&opt.iface, XdpFlags::default())
         .expect("Nie udało się podpiąć programu XDP do interfejsu");
 
-    info!(
-        "Firewall załadowany na interfejsie: {}. Naciśnij Ctrl-C, aby zakończyć.",
-        opt.iface
-    );
+    info!("Firewall załadowany na interfejsie: {}", opt.iface);
+
+    let mut blocklist: HashMap<_, u32, u32> = HashMap::try_from(bpf.map_mut("BLOCKLIST").unwrap())?;
+
+    if let Some(ip_str) = opt.block_ip {
+        let ipv4_adder = ip_str.parse::<Ipv4Addr>().unwrap();
+        let ip_u32 = u32::from(ipv4_adder);
+        blocklist.insert(ip_u32, 1, 0)?;
+        info!("Blocklist: {}", ipv4_adder);
+    }
 
     info!("Oczekiwanie na sygnał SIGINT...");
     signal::ctrl_c().await?;
