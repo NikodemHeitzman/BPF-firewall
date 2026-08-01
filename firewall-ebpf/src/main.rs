f00349dc36c9803f872ba00d37a089e9a37ec230
nikodem <nikodem.heitzman2002@gmail.com>
2026-08-01 17:23:41 +0200

    create hashmap and is_blocked

--- firewall-ebpf/src/main.rs
+++ firewall-ebpf/src/main.rs
@@ -1,12 +1,16 @@
 #![no_std]
 #![no_main]
 
-use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
+use aya_ebpf::{
+    bindings::xdp_action, macros::map, macros::xdp, maps::HashMap, programs::XdpContext,
+};
 use aya_log_ebpf::info;
 use core::ptr::read_unaligned;
 
 const ETH_P_IP: u16 = 0x0800;
 const ETH_HDR_LEN: usize = 14;
+#[map]
+static BLOCKLIST: HashMap<u32, u32> = HashMap::with_max_entries(1024, 0);
 
 #[xdp]
 pub fn firewall(ctx: XdpContext) -> u32 {
@@ -28,14 +32,18 @@ fn try_firewall(ctx: XdpContext) -> Result<u32, ()> {
     let ethertype = u16::from_be(unsafe { core::ptr::read_unaligned(ethertype_ptr) });
 
     if ethertype == ETH_P_IP {
-        info!(&ctx, "Otrzymano pakiet IPv4!");
-
         if data + 34 > data_end {
             return Ok(xdp_action::XDP_PASS);
         }
         let src_ip_ptr = (data + 26) as *const u32;
         let src_ip = u32::from_be(unsafe { core::ptr::read_unaligned(src_ip_ptr) });
-        // info!(&ctx, "Source IP: {:i}", src_ip); // Format {:i} to bajer aya-log do ładnego drukowania IP
+        info!(&ctx, "Source IP: {:i}", src_ip);
+
+        let is_blocked = unsafe { BLOCKLIST.get(&src_ip) };
+
+        if let Some(is_blocked) = is_blocked {
+            return Ok(xdp_action::XDP_DROP);
+        }
     }
 
     Ok(xdp_action::XDP_PASS)
